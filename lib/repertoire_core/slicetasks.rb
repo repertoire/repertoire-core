87c820181faf21cb46683e387196dae27b4ba47d
Dave Della Costa <davedellacosta@SLEBINOS.MIT.EDU>
2009-08-05 13:38:12 -0400

    Updated to work with merb 1.1 (edge).  Making this a separate branch since we don't know how much the new version of merb may change between now and when it is actually released.

--- lib/repertoire_core/slicetasks.rb
+++ lib/repertoire_core/slicetasks.rb
@@ -23,28 +23,33 @@ namespace :slices do
       # NOTE.  dependency on merb_env causes merb to load so models available
       #        however, we connnect as 'postgres' just for the purposes of creating the 
       #        model tables and granting access to the project
-      default_uri = DataMapper.repository(:default).adapter.uri
-      core_uri    = DataMapper.repository(:core).adapter.uri
-      project     = default_uri.user
+      default_uri = DataMapper.repository(:default).adapter.options
+      core_uri    = DataMapper.repository(:core).adapter.options
+      project     = default_uri[:user]
       
       Merb.logger.info "Detected your Hyperstudio project abbreviation is '#{project}'"
 
       # error check database configuration
       case
-      when default_uri.path != core_uri.path then raise "Repertoire and project-level tables should be in the same database"
-      when default_uri.user == 'postgres'    then raise "Project-level tables should have their own schema and user for access"
-      when default_uri.user == core_uri.user then raise "Repertoire core tables should not be stored in the project-level database schema"
+      when default_uri[:path] != core_uri[:path] then raise "Repertoire and project-level tables should be in the same database"
+      when default_uri[:user] == 'postgres'    then raise "Project-level tables should have their own schema and user for access"
+      when default_uri[:user] == core_uri[:user] then raise "Repertoire core tables should not be stored in the project-level database schema"
       end
 
       # create repertoire tables transactionally in the public schema      
       repository(:core) do
         DataMapper::Transaction.new do |txn|
           core_models = [ User, Role, Membership ]
+
+          # raise core_uri.inspect
         
-          Merb.logger.info "Migrating Repertoire Core models into #{core_uri}"
+          Merb.logger.info "Migrating Repertoire Core models into #{core_uri.inspect}"
           core_models.each do |m| 
+
+            # raise "ALTER TABLE #{m.storage_name} OWNER TO #{core_uri[:user]}, ALTER SEQUENCE #{m.storage_name}_id_seq OWNED BY #{m.storage_name}.id, GRANT ALL ON #{m.storage_name} TO #{project}, GRANT ALL ON #{m.storage_name}_id_seq TO #{project}".inspect
+
             m.auto_migrate!
-            [ "ALTER TABLE #{m.storage_name} OWNER TO #{core_uri.user}",
+            [ "ALTER TABLE #{m.storage_name} OWNER TO #{core_uri[:user]}",
               "ALTER SEQUENCE #{m.storage_name}_id_seq OWNED BY #{m.storage_name}.id",
               "GRANT ALL ON #{m.storage_name} TO #{project}",
               "GRANT ALL ON #{m.storage_name}_id_seq TO #{project}" ].each { |stmt| repository(:core).adapter.execute(stmt) }
